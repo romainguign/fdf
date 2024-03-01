4e6a7f456c250ba4bd9b3aadfb3111310cddd5cc
romainguign <guignard.romain@protonmail.com>
2024-03-01 14:49:34 +0100

    separed bonus

---
+++ libft/sources/ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roguigna <roguigna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/05 14:55:02 by roguigna          #+#    #+#             */
+/*   Updated: 2023/11/30 14:28:34 by roguigna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*tmp;
+
+	if (!lst || !del)
+		return ;
+	while (*lst)
+	{
+		tmp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		(*lst) = tmp;
+	}
+}
---
+++ libft/sources/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roguigna <roguigna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/04 13:53:11 by roguigna          #+#    #+#             */
+/*   Updated: 2023/11/30 14:28:34 by roguigna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*element_lst;
+
+	element_lst = malloc(sizeof(t_list));
+	if (!element_lst)
+		return (0);
+	element_lst->content = content;
+	element_lst->next = NULL;
+	return (element_lst);
+}
